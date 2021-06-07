46151132b4a7dc72544feb8d0128acc4f6676435
ALewdDev <ALewdDev@gmail.com>
2021-06-07 11:14:58 -0600

    Shortcut Color Demo

--- JS/Shortcuts.js
+++ JS/Shortcuts.js
@@ -5,8 +5,44 @@
 
 {
   let activeCategory;
+  let controls;
 
   const getCategory = () => document.getElementById(`category-${ activeCategory }`);
+
+
+  const shortcuts = {
+    Search: {
+      list: [{
+        name: 'Previous Page',
+        color: '#d8b03e',
+        buttons: [ 'A' , '🠔' ]
+      },{
+        name: 'Next Page',
+        color: '#d8b03e',
+        buttons: [ 'D' , '🠖' ]
+      },{
+        name: 'Search',
+        color: '#a956bd',
+        buttons: [ 'Q' ]
+      },{
+        name: 'Scroll Up',
+        color: '#59b9b0',
+        buttons: [ 'W' ]
+      },{
+        name: 'Scroll Down',
+        color: '#59b9b0',
+        buttons: [ 'S' ]
+      },{
+        name: 'Random Post',
+        color: '#28c366',
+        buttons: [ 'R' ]
+      },{
+        name: 'Switch Tag',
+        color: '#f554a6',
+        buttons: [ '1' , '2' , '3' ]
+      }]
+    }
+  };
 
 
   toggleCategory = (category) => {
@@ -17,5 +53,35 @@
 
     getCategory()?.classList.add('selectedCategory');
 
+    const keys = shortcuts[activeCategory];
+
+    controls = new Map;
+
+    if(keys)
+      keys.list.forEach(({ name , action , buttons , color }) => {
+        buttons.forEach((key) => {
+          controls.set(key,{ action , color });
+        })
+      });
+
+    // console.log(controls);
+
+
+    for(const element of document.getElementsByTagName('Key')){
+      const name = element.innerText;
+
+      let Color = '';
+
+      // console.log(name);
+
+      if(controls.has(name)){
+        const { color , action } = controls.get(name);
+
+        Color = color ?? 'var(--white)';
+      }
+
+      element.style.backgroundColor = Color;
+      element.style.color = Color ? 'white' : '';
+    }
   }
 }
